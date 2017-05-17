2a729e890e0294bc09f1155770f4a1d625328909
Mateus Santiago <mateussantiago333@gmail.com>
2017-05-17 11:23:15 -0300

    Teste

--- src/Ens/JobeetBundle/Form/EstabelecimentoType.php
+++ src/Ens/JobeetBundle/Form/EstabelecimentoType.php
@@ -14,7 +14,7 @@ class EstabelecimentoType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        //$builder->add('nome_estabelecimento',null,array('label' => 'Nome do estabelecimento:'));
+        $builder->add('nome_estabelecimentoeco',null,array('label' => 'Nome do estabelecimento:'));
         $builder->add('tipo_estabelecimento', ChoiceType::class, array('label' => 'Tipo do estabelecimento:',
         'choices'  => array(
         'Bar' => 'Bar',
