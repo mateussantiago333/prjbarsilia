2bdfbd2567962e7756fc181ce037f6940845bddc
Mateus Santiago <mateussantiago333@gmail.com>
2017-05-21 20:06:41 -0300

    Inserindo Starbundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -48,4 +48,12 @@ class AppKernel extends Kernel
     {
         $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
     }
+
+    public function registerBundles()
+    {
+    $bundles = array(
+        // ...
+        new blackknight467\StarRatingBundle\StarRatingBundle(),
+    );
+    }
 }
