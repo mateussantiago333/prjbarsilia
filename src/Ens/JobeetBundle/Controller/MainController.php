b7820e47c64e613075f5a53468bd2202b401afa9
Mateus Santiago <mateussantiago333@gmail.com>
2017-05-27 21:12:53 -0300

    Adição de estabelecimentos cadastrados na Main page

--- src/Ens/JobeetBundle/Controller/MainController.php
+++ src/Ens/JobeetBundle/Controller/MainController.php
@@ -6,6 +6,10 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Request;
+use Ens\JobeetBundle\Entity\Comentario;
+use Ens\JobeetBundle\Entity\Estabelecimento;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\Collections\ArrayCollection;
 
 
 class MainController extends Controller
@@ -13,6 +17,19 @@ class MainController extends Controller
 
     public function indexAction()
     {
+        $em = $this->getDoctrine()->getManager();
+        $query = $em->createQuery(
+            ('SELECT 
+            	e.id,e.nome_estabelecimento,AVG(c.nota) as nota_media,
+            						   e.descricao, e.url_img,e.cidade
+            	 FROM EnsJobeetBundle:Estabelecimento e 
+            	 LEFT JOIN e.comentario c 
+            	 GROUP BY e.id 
+            	 ORDER BY nota_media DESC'))->setMaxResults(5);
+        $estabelecimentos = $query->getResult();
+
+    	shuffle($estabelecimentos);
+
 		return $this->render('main/main.html.twig');
     }
 }
