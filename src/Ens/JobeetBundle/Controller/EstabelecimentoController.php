192f41556adab8cc8258e3afe68e7d174a924593
Mateus Santiago <mateussantiago333@gmail.com>
2017-05-28 18:10:14 -0300

    Mudança nos filtros

--- src/Ens/JobeetBundle/Controller/EstabelecimentoController.php
+++ src/Ens/JobeetBundle/Controller/EstabelecimentoController.php
@@ -24,7 +24,7 @@ class EstabelecimentoController extends Controller
         $cidade = $request->get('search_cidade');
         $tipo = $request->get('search_tipo');
 
-    if ($cidade>isSubmitted()) {
+    if (!empty($cidade)) {
         $query_filter = $em->createQuery('SELECT e.id,e.nome_estabelecimento,AVG(c.nota) as nota_media,
         e.descricao, e.url_img,e.cidade FROM EnsJobeetBundle:Estabelecimento e LEFT JOIN e.comentario c 
         WHERE e.cidade = :cidade
