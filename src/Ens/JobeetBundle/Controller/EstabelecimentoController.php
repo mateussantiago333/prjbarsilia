d36fdda20ecd27a5fb7b80f32ab80360e8766f1f
Mateus Santiago <mateussantiago333@gmail.com>
2017-05-28 14:33:19 -0300

    Mudança nos filtros

--- src/Ens/JobeetBundle/Controller/EstabelecimentoController.php
+++ src/Ens/JobeetBundle/Controller/EstabelecimentoController.php
@@ -23,12 +23,10 @@ class EstabelecimentoController extends Controller
         $em = $this->getDoctrine()->getManager();
         $cidade = $request->get('search_cidade');
 
-
         //$estabelecimentos = $em->getRepository('EnsJobeetBundle:Estabelecimento')->findAll();
         $query = $em->createQuery('SELECT e.id,e.nome_estabelecimento,AVG(c.nota) as nota_media,
                 e.descricao, e.url_img,e.cidade FROM EnsJobeetBundle:Estabelecimento e LEFT JOIN e.comentario c GROUP BY e.id ORDER BY nota_media DESC');
         $estabelecimentos = $query->getResult();
-
         return $this->render('estabelecimento/index.html.twig', array(
             'estabelecimentos' => $estabelecimentos,
         ));
