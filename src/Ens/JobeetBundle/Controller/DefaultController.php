d8dd194def78925d2ba56782414e255087eab07f
Mateus Santiago <mateussantiago333@gmail.com>
2017-05-27 22:16:34 -0300

    Adição de estabelecimentos cadastrados na Main page

--- src/Ens/JobeetBundle/Controller/DefaultController.php
+++ src/Ens/JobeetBundle/Controller/DefaultController.php
@@ -3,11 +3,32 @@
 namespace Ens\JobeetBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
+use Ens\JobeetBundle\Entity\Comentario;
+use Ens\JobeetBundle\Entity\Estabelecimento;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\Collections\ArrayCollection;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('EnsJobeetBundle:Default:index.html.twig');
+        $em = $this->getDoctrine()->getManager();
+        $query = $em->createQuery(
+            ('SELECT 
+            	e.id,e.nome_estabelecimento,AVG(c.nota) as nota_media,
+            						   e.descricao, e.url_img,e.cidade
+            	 FROM EnsJobeetBundle:Estabelecimento e 
+            	 LEFT JOIN e.comentario c 
+            	 GROUP BY e.id ORDER BY nota_media DESC'))->setMaxResults(3);
+        $estabelecimentos = $query->getResult();
+    	//shuffle($estabelecimentos);
+
+		return $this->render('main/main.html.twig', array(
+            'estabelecimentos' => $estabelecimentos,
+        ));
     }
 }
