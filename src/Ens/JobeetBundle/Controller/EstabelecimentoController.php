9b1881f75bd27ecd96837dcc9e63aa7da5e77703
Mateus Santiago <mateussantiago333@gmail.com>
2017-05-28 19:04:55 -0300

    Mudança nos filtros

--- src/Ens/JobeetBundle/Controller/EstabelecimentoController.php
+++ src/Ens/JobeetBundle/Controller/EstabelecimentoController.php
@@ -35,6 +35,14 @@ class EstabelecimentoController extends Controller
          GROUP BY e.id ORDER BY nota_media DESC')
         ->setParameter('cidade', $cidade)->setParameter('tipo', $tipo);
         $estabelecimentos = $query_filter->getResult();
+    }elseif (!empty($tipo)) {
+        $query_filter = $em->createQuery('SELECT e.id,e.nome_estabelecimento,AVG(c.nota) as nota_media,
+        e.descricao,e.tipo_estabelecimento, e.url_img,e.cidade
+         FROM EnsJobeetBundle:Estabelecimento e LEFT JOIN e.comentario c 
+        WHERE e.tipo_estabelecimento = :tipo
+         GROUP BY e.id ORDER BY nota_media DESC')
+        ->setParameter('cidade', $cidade)->setParameter('tipo', $tipo);
+        $estabelecimentos = $query_filter->getResult();
     }elseif (!empty($cidade)) {
         $query_filter = $em->createQuery('SELECT e.id,e.nome_estabelecimento,AVG(c.nota) as nota_media,
         e.descricao,e.tipo_estabelecimento, e.url_img,e.cidade
