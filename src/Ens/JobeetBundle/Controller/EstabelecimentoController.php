e909204098f63daa80ac0ce4a19019bf72cfff6b
Mateus Santiago <mateussantiago333@gmail.com>
2017-05-28 18:17:44 -0300

    Mudança nos filtros

--- src/Ens/JobeetBundle/Controller/EstabelecimentoController.php
+++ src/Ens/JobeetBundle/Controller/EstabelecimentoController.php
@@ -24,20 +24,20 @@ class EstabelecimentoController extends Controller
         $cidade = $request->get('search_cidade');
         $tipo = $request->get('search_tipo');
 
-    if (!empty($cidade)) {
+    if (!empty($cidade) || !empty($tipo)) {
         $query_filter = $em->createQuery('SELECT e.id,e.nome_estabelecimento,AVG(c.nota) as nota_media,
         e.descricao, e.url_img,e.cidade FROM EnsJobeetBundle:Estabelecimento e LEFT JOIN e.comentario c 
         WHERE e.cidade = :cidade
          GROUP BY e.id ORDER BY nota_media DESC')->setParameter('cidade', $cidade);
         $estabelecimentos = $query_filter->getResult();
     }
-    if(empty($cidade)){
+    if(empty($cidade) && empty($tipo)){
         //$estabelecimentos = $em->getRepository('EnsJobeetBundle:Estabelecimento')->findAll();
         $query = $em->createQuery('SELECT e.id,e.nome_estabelecimento,AVG(c.nota) as nota_media,
                 e.descricao, e.url_img,e.cidade FROM EnsJobeetBundle:Estabelecimento e LEFT JOIN e.comentario c GROUP BY e.id ORDER BY nota_media DESC');
         $estabelecimentos = $query->getResult();
     }
-    
+
         return $this->render('estabelecimento/index.html.twig', array(
             'estabelecimentos' => $estabelecimentos,
         ));
