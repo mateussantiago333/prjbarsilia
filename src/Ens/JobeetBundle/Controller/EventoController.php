4d881a3b967e932a8f2729ea727e8994426309c7
Mateus Santiago <mateussantiago333@gmail.com>
2017-06-02 19:52:02 -0300

    Alterações de index

--- src/Ens/JobeetBundle/Controller/EventoController.php
+++ src/Ens/JobeetBundle/Controller/EventoController.php
@@ -24,8 +24,8 @@ class EventoController extends Controller
                                             AVG(c.nota) as nota_evento,
                                             ev.descricao_evento, ev.img_evento,
                                             ev.data_evento, e.nome_estabelecimento
-                                    FROM EnsJobeetBundle:Estabelecimento e
-                                    LEFT JOIN e.evento ev
+                                    FROM EnsJobeetBundle:Evento ev
+                                    JOIN e.estabelecimentos e
                                     LEFT JOIN ev.comentario_evento c
                                     GROUP BY ev.id,e.id ORDER BY ev.data_evento,nota_evento DESC');
         $eventos = $query_eventos->getResult();
